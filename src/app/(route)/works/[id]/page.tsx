3ba2e4424f40667dbb397e248cd6c286d76dfcb5
HamsterStudent <hwanyamyu@gmail.com>
2024-02-07 15:15:07 +0900

    :construction: Feat : add detail page

--- src/app/(route)/works/[id]/page.tsx
+++ src/app/(route)/works/[id]/page.tsx
@@ -1,13 +1,68 @@
 "use client";
 
+import axios from "axios";
 import { useRouter, usePathname } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
+interface IData {
+  id: string;
+  name: string;
+  tag: string[];
+  img: string[];
+  size: number;
+  exp: string;
+  description: string;
+}
 
 export default function Detail() {
   const router = useRouter();
-  const pathnName = usePathname();
+  const pathnName = usePathname().replace("/works/", "");
+  const [data, setData] = useState<IData>();
 
-  console.log(pathnName);
-
-  return <div>{pathnName}</div>;
+  useEffect(() => {
+    fetchData();
+  }, [data]);
+  const fetchData = async () => {
+    try {
+      const response = await axios.post("/api/works", {
+        query: `
+        query {
+          product(id: "${pathnName}") {
+            description
+            exp
+            id
+            img
+            name
+            size
+            tag
+          }
+        }
+      `,
+      });
+      const {
+        data: {
+          data: { product },
+        },
+      } = response;
+      setData(product);
+    } catch (error) {
+      console.error("error:", error);
+    }
+  };
+  return (
+    <div>
+      {data ? (
+        <>
+          <div>
+            <h1>{data.name}</h1>
+            <div>{data.description}</div>
+            <div>{data.exp}</div>
+            <div>
+              <img src="/assets/prod/6.png" alt={`${data.name}`} />
+            </div>
+          </div>
+        </>
+      ) : null}
+    </div>
+  );
 }
